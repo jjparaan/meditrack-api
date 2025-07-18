3a9b25a521f0608128802bcdaf5411f637ca6f33
JJ <jparaan@rocketmail.com>
2025-07-18 19:31:03 +0800

    [Persistence] Centralize project DI registration
    1. This and 2 commits before this are correlated

---
+++ MediTrack.Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using MediTrack.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediTrack.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
