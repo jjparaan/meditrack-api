e05331525f094b9ac7243aab6cb086c92ce80f2b
JJ <jparaan@rocketmail.com>
2025-07-18 19:29:23 +0800

    [Application] Centralize project DI registration

---
+++ MediTrack.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MediTrack.Application.Common.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediTrack.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
